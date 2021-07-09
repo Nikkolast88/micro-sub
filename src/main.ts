99c25b030c1d95c645bb7e5a142d26b7a8d63888
dengriguang <1971167057@qq.com>
2021-07-09 09:43:15 +0800

    Feat:初始化

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,31 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
+import './public-path';
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import { Prop } from './typings';
 
-createApp(App).use(router).mount("#app");
+// createApp(App).use(router).mount('#app');
+let instance = null;
+
+function render(props: Prop) {
+  const { container } = props;
+  instance = createApp(App);
+  instance.use(router);
+  instance.mount(container ? container.querySelector('#app') : '#app');
+}
+
+if (!window.__POWERED_BY_QIANKUN__) {
+  render({ container: '' });
+}
+export async function bootstrap() {
+  console.log('%c ', 'color: green;', 'vue3.0 app bootstraped');
+}
+
+export async function mount(props: Prop) {
+  render(props);
+}
+
+export async function unmount() {
+  // instance.unmount();
+  // instance._container.innerHTML = '';
+}
---
+++ vue.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+// const { name } = require('./package');
+const { name } = require('./package.json');
+
+function resolve(dir) {
+  return path.join(__dirname, dir);
+}
+
+module.exports = {
+  publicPath: 'child/vue3',
+  outputDir: 'dist',
+  assetsDir: 'static',
+  filenameHashing: true,
+  devServer: {
+    hot: true,
+    disableHostCheck: true,
+    port: 3000,
+    overlay: {
+      warnings: false,
+      errors: true,
+    },
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+  },
+  // 自定义webpack配置
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': resolve('src'),
+      },
+    },
+    output: {
+      // 把子应用打包成 umd 库格式
+      library: `${name}-[name]`,
+      libraryTarget: 'umd',
+      jsonpFunction: `webpackJsonp_${name}`,
+    },
+  },
+};
